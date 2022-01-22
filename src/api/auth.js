735b71983b956806f4ffc708120d6ed776f3f417
kirkmonz <kirkemmons2@gmail.com>
2022-01-22 14:15:29 -0500

    updateHeader.js file

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,55 +1,61 @@
-import apiUrl from '../apiConfig'
 import axios from 'axios'
+import apiUrl from '../apiConfig'
 
-export const signUp = (credentials) => {
-  return axios({
-    method: 'POST',
-    url: apiUrl + '/sign-up/',
-    data: {
-      credentials: {
-        email: credentials.email,
-        password: credentials.password,
-        password_confirmation: credentials.passwordConfirmation
-      }
+// We want to export multiple things from this file, so we are going to use
+// a named export for the function `signUp`, instead of a `default` export
+// (which would only allow 1 export per file)
+export const signUp = (email, password, passwordConfirmation) => {
+  // make an axios call to signUp, make sure to return
+  // so we can use `.then`/`.catch` (or async/await)
+  // in SignUp.js
+  return axios.post(apiUrl + '/sign-up/', {
+    credentials: {
+      email,
+      password,
+      password_confirmation: passwordConfirmation
     }
   })
 }
 
-export const signIn = (credentials) => {
-  return axios({
-    url: apiUrl + '/sign-in/',
-    method: 'POST',
-    data: {
-      credentials: {
-        email: credentials.email,
-        password: credentials.password
-      }
+export const signIn = (email, password) => {
+  // make an axios call to signUp, make sure to return
+  // so we can use `.then`/`.catch` (or async/await)
+  // in SignUp.js
+  return axios.post(apiUrl + '/sign-in/', {
+    credentials: {
+      email,
+      password
+    }
+  })
+}
+
+export const changePassword = (oldPassword, newPassword, user) => {
+  // make an axios call to signUp, make sure to return
+  // so we can use `.then`/`.catch` (or async/await)
+  // in SignUp.js
+  return axios.patch(apiUrl + '/change-password/', {
+    passwords: {
+      old: oldPassword,
+      new: newPassword
+    }
+  },
+  {
+    headers: {
+      Authorization: `Bearer ${user.token}`
     }
   })
 }
 
 export const signOut = (user) => {
-  return axios({
-    url: apiUrl + '/sign-out/',
-    method: 'DELETE',
-    headers: {
-      Authorization: `Bearer ${user.token}`
-    }
-  })
-}
-
-export const changePassword = (passwords, user) => {
-  return axios({
-    url: apiUrl + '/change-password/',
-    method: 'PATCH',
-    headers: {
-      Authorization: `Bearer ${user.token}`
-    },
-    data: {
-      passwords: {
-        old: passwords.oldPassword,
-        new: passwords.newPassword
+  // make an axios call to signUp, make sure to return
+  // so we can use `.then`/`.catch` (or async/await)
+  // in SignUp.js
+  return axios.delete(
+    apiUrl + '/sign-out/',
+    {
+      headers: {
+        Authorization: `Bearer ${user.token}`
       }
     }
-  })
+  )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,70 @@
-import React, { Fragment } from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import { Link, NavLink } from 'react-router-dom'
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
+// import Navbar from 'react-bootstrap/Navbar'
+// import Container from 'react-bootstrap/Container'
+// import Nav from 'react-bootstrap/Nav'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+// These are the links you'll see if you're signed in
 const authenticatedOptions = (
-  <Fragment>
-    <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
-    <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
-  </Fragment>
+  <>
+    <NavLink to='/change-password' className='nav-link'>
+Change Password
+    </NavLink>
+    <NavLink to='/sign-out' className='nav-link'>
+Sign Out
+    </NavLink>
+    <NavLink to='/posts' className='nav-link'>
+Posts
+    </NavLink>
+    <NavLink to='/posts/create' className='nav-link'>
+Make a Post
+    </NavLink>
+  </>
 )
 
+// These are the links you'll see if you're signed out
 const unauthenticatedOptions = (
-  <Fragment>
+  <>
     <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
     <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
-  </Fragment>
+  </>
 )
 
+// These are the navigation links that will always show
 const alwaysOptions = (
-  <Fragment>
-    <NavLink exact to='/' className='nav-link'>Home</NavLink>
-  </Fragment>
+  <>
+    <NavLink to='/' className='nav-link'>Home</NavLink>
+  </>
 )
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
-    <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}></Link>
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls='basic-navbar-nav' />
-    <Navbar.Collapse id='basic-navbar-nav'>
-      <Nav className='ml-auto'>
-        {user && (
-          <span className='navbar-text mr-2'>Welcome, {user.email}</span>
-        )}
-        {alwaysOptions}
-        {user ? authenticatedOptions : unauthenticatedOptions}
-      </Nav>
-    </Navbar.Collapse>
+  <Navbar bg='info' expand='md' variant='dark'>
+    <Container>
+      <Navbar.Brand>
+        {/* Text decoration: none, will remove the underline text decoration
+            from the link */}
+        <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>myMDB
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        {/* Add margin to the start (left) automatically
+            So that the links are pushed to the right side of the screen. */}
+        <Nav className='ms-auto'>
+          {/* If we have a user, show the span component */}
+          {user && (
+          /* Add some margin after the welcome message to separate it from the following links */
+            <span className='navbar-text me-2'>Welcome, {user.email}</span>
+          )}
+          {alwaysOptions}
+          {/* If we have a user (it isnt null), show the authenticated links
+              otherwise show the unauthenticated links */}
+          {user ? authenticatedOptions : unauthenticatedOptions}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 )
 
