946d92d7a2d150cb1f64bfdac68a4ae2afc78fcd
kirkmonz <kirkemmons2@gmail.com>
2022-01-25 23:25:44 -0500

    comment routes frontend

---
+++ src/api/comments.js
@@ -0,0 +1,31 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createComment = (body) => {
+  return axios({
+    url: apiUrl + '/comments/',
+    method: 'POST',
+    data: {
+      comment: {
+        text: body.text,
+        owner: body.owner
+      }
+    }
+  })
+}
+
+export const changeComment = (id, text) => {
+  return axios({
+    url: apiUrl + `/comments/${id}`,
+    method: 'PATCH',
+    comment: { text }
+  })
+}
+
+export const deleteComment = (id, user) => {
+  return axios({
+    url: apiUrl + `/comments/${id}`,
+    method: 'DELETE',
+    headers: { Authorization: `Bearer ${user.token}` }
+  })
+}
